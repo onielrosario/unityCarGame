493504a6e674eaace0ea8a6df9a57589954ce4e5
onielrosario <onielrosario@pursuit.org>
2019-11-04 14:04:59 -0700

    initial commit
    initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private UnityEngine.Vector3 offset = new UnityEngine.Vector3(0, 5, -7);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position = player.transform.position + offset;  
+    }
+}
